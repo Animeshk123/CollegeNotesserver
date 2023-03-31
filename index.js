a2b805af114839bb7cc0fc138560c356fe6eaf12
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-03-31 14:52:26 +0530

    changed some routes

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1",router);
 
+app.get("/",(req,res) => {
+    res.send("hello from server");
+})
+
 
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
